b52999f8efcbfb84942f8d80354e6e836f4012b3
Quanye Chen <quanyec@gmail.com>
2021-10-24 17:20:08 +0800

    Fix gnu elpa link

--- init.el
+++ init.el
@@ -4,13 +4,13 @@
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; You may delete these explanatory comments.https://github.com/AiziChen/emacs.d/blob/master/init.el
 (require 'package)
 ;;(add-to-list 'package-archives
 ;;	     '("melpa" . "https://melpa.org/packages/") t)
 (setq package-archives
       '(("melpa" . "http://elpa.emacs-china.org/melpa/")
-	      ("gun" . "http://elpa.emacs-china.org/melpa/")))
+	("gun" . "http://elpa.emacs-china.org/gnu/")))
 ;;(package-refresh-contents)
 (package-initialize)
 ;; auto-load the packages path

4d609959152d900daf8e9d6ff95c6e7c9f65a6eb
AiziChen <1098679804@qq.com>
2024-08-08 23:09:25 +0800

    mode: add erlang mode

--- init.el
+++ init.el
@@ -143,7 +143,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(flycheck lsp-ui lsp-mode racket-mode ace-window paredit company org-bullets)))
+   '(erlang flycheck lsp-ui lsp-mode racket-mode ace-window paredit company org-bullets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -292,3 +292,8 @@
             ;(local-set-key (kbd "M-q") 'swish-indent-sexp)
             ))
 
+(use-package erlang
+  :ensure t
+  :init
+  (progn
+    (add-hook 'erlang-mode-hook #'lsp)))
